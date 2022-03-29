d02e72b41b0fe1b67b290d0bc0ee897768bcf492
Azhari <Azhari19ti>
2022-03-29 17:27:44 +0700

    form

--- app/Http/Controllers/RTController.php
+++ app/Http/Controllers/RTController.php
@@ -6,6 +6,7 @@ use Ramsey\Uuid\Uuid;
 use App\Models\RT;
 use Illuminate\Http\Request;
 
+
 class RTController extends Controller
 {
     /**
@@ -44,36 +45,15 @@ class RTController extends Controller
     public function store(Request $request)
     { 
         $validatedData = $request->validate([
-            'nik' => 'numeric|min:16',
-            'kk' => 'numeric|min:16',
-            'name' => 'required|max:255',
-            'date_birth' => 'required|date',
-            'place_birth' => 'required',
-            'religion' => 'required',
-            'job' => 'required',
-            'phone' => 'numeric|required',
-            'marriage' => 'required',
-            'move_date' => 'date|nullable',
-            'death_date' => 'date|nullable',
-            'gender' => 'required',
-            'family_status' => 'required',
-            'blood' => 'required',
-            'vaccine_1' => 'nullable',
-            'vaccine_2' => 'nullable',
-            'vaccine_3' => 'nullable',
-            'rt' => 'nullable',
-            'rw' => 'nullable',
-            'village' => 'nullable',
-            'sub_districts' => 'nullable',
-            'districts' => 'nullable',
-            'province' => 'nullable',
+            'name' => 'string|required|max:255'
+            
         ]);
 
         $validatedData['uuid'] = Uuid::uuid4()->getHex();
 
         RT::create($validatedData);
 
-        return redirect('/citizens')->with('success','Data kependudukan berhasil ditambah!');
+        return redirect('/rt')->with('success','Data RT berhasil ditambah!');
     }
 
     /**
--- resources/views/masters/rt/form.blade.php
+++ resources/views/masters/rt/form.blade.php
@@ -4,13 +4,15 @@
     <div class="card">
       <div class="card-body">
         <h4 class="card-title">Input Data RT</h4>
+        <form class="forms-sample" action="/rt" method="POST">          
+          @csrf
         <p class="card-description">
           Input Data RT
         </p>
-        <form class="forms-sample" action="/masters/rt/form">          
+        
           <div class="form-group">
             <label for="exampleInputEmail3">Nama RT</label>
-            <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
+            <input type="text" class="form-control" id="exampleInputEmail3" placeholder="RT">
           </div>
           <button type="submit" class="btn btn-primary me-2">Submit</button>
           <button class="btn btn-light">Cancel</button>
