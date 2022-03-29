56cc18dd64318ea38fa50ae18bb39b187bbf5fa6
ranubim <ranubimcode@gmail.com>
2022-03-29 15:42:14 +0700

    Modul Assistance

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,4 @@ Route::resource("citizens", "App\Http\Controllers\Transactions\CitizenController
 Route::resource("users", "App\Http\Controllers\UserController");
 
 //jika penggunaan resource, path harus lengkap
-<<<<<<< HEAD
 Route::resource("assistance", "App\Http\Controllers\AssistanceController");
-=======
-Route::resource("rt", "App\Http\Controllers\RTController");
->>>>>>> 07808c3dc53d94fbe2fd2dc8f0f8c046680d01b7
