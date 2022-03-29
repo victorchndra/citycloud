39e2abcffcf24d5bcc35aef67dc6f169d6d5e221
victorchandra0911 <victorchandra0911@gmail.com>
2022-03-29 15:44:33 +0700

    perbaiki seeder dan migration RW

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CitizenTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(RTSeeder::class);
+        $this->call(DatarwSeeder::class);
     }
 }
