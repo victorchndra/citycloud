aa87026f8d92873c33b7c57a4218281054dffdcf
hoscloudit <hoscloudit@gmail.com>
2022-03-30 13:18:50 +0700

    login module

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//pastikan aktifkan ini utk cek auth loginya
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('auth.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function authenticate(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'username'=> 'required',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+           
+            $request->session()->regenerate();
+            // dd(Auth::id());
+            return redirect()->intended('/');
+        }
+
+        return back()->with('loginError', 'Username atau Password salah');
+    }
+
+
+    public function logout(Request $request)
+    {
+    Auth::logout();
+ 
+    $request->session()->invalidate();
+ 
+    $request->session()->regenerateToken();
+ 
+    return redirect('/');
+    }
+    
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/auth/index.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Spica Admin</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="../../vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../../css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../../images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller d-flex">
+    <div class="container-fluid page-body-wrapper full-page-wrapper d-flex">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="../../images/logo-dark.svg" alt="logo">
+              </div>
+              <h4>Halo, Silahkan Login</h4>
+              <h6 class="font-weight-light">Isi username dan password anda.</h6>
+              
+              
+              <form method="POST" action="/login">
+                @csrf
+                <div class="form-group row">
+                    <div class="input-group">
+                        <input type="text" name="username" value="{{ old('username') }}" autocomplete="username" class="form-control @error('username') is-invalid @enderror" placeholder="Username"  autofocus required >
+                       
+                    </div>
+                    @error('username')
+                    <div class="input-group">
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    </div>
+                    @enderror
+                </div>
+                <div class="form-group row">
+                    <div class="input-group">
+                        <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+                       
+                    </div>
+                    <div class="input-group">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary submit-btn btn-block">Login</button>
+                </div>
+                <div class="text-block text-center my-3">
+                <span class="text-small font-weight-semibold">Hospicloud 2.0 Copyright By</span>
+                    <a href="https://hoscloud.co.id/" target="_blank" class="text-small">HOSCLOUD.CO.ID</a>
+                </div>
+            </form>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- base:js -->
+  <script src="../../vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <script src="../../js/jquery.cookie.js" type="text/javascript"></script>
+  <!-- inject:js -->
+  <script src="../../js/off-canvas.js"></script>
+  <script src="../../js/hoverable-collapse.js"></script>
+  <script src="../../js/template.js"></script>
+  <!-- endinject -->
+</body>
+
+</html>
