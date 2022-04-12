e3ea3af2fafa51e833da860a6be44bcf5ab51f0d
Kuhaaku51 <kuhaaku51@gmail.com>
2022-04-12 16:12:11 +0700

    fix index dan ui data informasi

--- resources/views/masters/information/index.blade.php
+++ resources/views/masters/information/index.blade.php
@@ -15,7 +15,7 @@
                 <p class="card-description">
                     Data Informasi
                 </p>
-                <a href="/information" class="btn btn-sm btn-secondary btnReload"><i class="mdi mdi-refresh"></i></a>
+                <a href="/ti" class="btn btn-sm btn-secondary btnReload"><i class="mdi mdi-refresh"></i></a>
                 
                 <div class="table-responsive pt-3">
                     <table class="table table-bordered">
@@ -47,10 +47,10 @@
                                 <td>{{ $data->created_at, 'H:i:s'}}</td>
                                 <td>
                                     <div class="btn-group-vertical" role="group" aria-label="Basic example">
-                                        <div class="btn-group">
+                                        <div class="btn-group d-flex">
                                             <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">Aksi</button>
                                             <div class="dropdown-menu">
-                                            <a href="/ti/{{ $data->uuid }}/edit" class="dropdown-item"><i class="bi bi-pencil-fill"></i>Edit</a>
+                                            <a href="/ti/{{ $data->uuid }}/edit" class="dropdown-item"><i class="mdi mdi-tooltip-edit"></i>Edit</a>
                                         </div>
                                     </div>
                                 </td>
