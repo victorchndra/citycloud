5335607446becd55601957d1d4eabfb0a328f6b2
victorchandra0911 <victorchandra0911@gmail.com>
2022-04-13 15:34:17 +0700

    dateBirth attribute

--- app/Models/Transactions/Citizens.php
+++ app/Models/Transactions/Citizens.php
@@ -69,8 +69,7 @@ class Citizens extends Model
     public function dateBirth(): Attribute
     {
         return new Attribute(
-            get: fn ($value) => Carbon::createFromFormat('Y-m-d', $this->attributes['date_birth'])->isoFormat('D MMMM Y'),
-
+            get: fn ($value) => Carbon::createFromFormat('Y-m-d', $this->attributes['date_birth'])->isoFormat('D MMMM Y') . ' <strong>['. Carbon::parse($this->attributes['date_birth'])->age . ' th]</strong>',
         );
     }
 
