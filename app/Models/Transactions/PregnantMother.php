0b34e0490a866fd6715118253744df77292904ab
Kuhaaku [ ] <kuhaaku51@gmail.com>
2022-06-13 10:49:35 +0700

    data ibu hamil

---
+++ app/Models/Transactions/PregnantMother.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PregnantMother extends Model
+{
+    use HasFactory;
+}
