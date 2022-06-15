b74902b42c9450635f9e37325a8d841d150dbe69
victorchandra0911 <victorchandra0911@gmail.com>
2022-06-15 14:23:33 +0700

    Edit form dan perbaiki index KMS data anak

---
+++ app/Http/Controllers/ChildrenController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transactions\Children;
+use Illuminate\Http\Request;
+
+class ChildrenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Transactions\Children  $children
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Children $children)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Transactions\Children  $children
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Children $children)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Transactions\Children  $children
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Children $children)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Transactions\Children  $children
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Children $children)
+    {
+        //
+    }
+}
